5ae7e8e0c89d8b38567ce2b63a49b62bde1a9241
Kavyavineela <kavyavineelakillada@gmail.com>
2026-07-24 11:51:44 +0530

    Fix provider issues and restore Password Vault

--- flutter_app/lib/config/app.dart
+++ flutter_app/lib/config/app.dart
@@ -4,18 +4,22 @@ import 'package:provider/provider.dart';
 import '../features/authentication/presentation/screens/login_screen.dart';
 import '../features/password_vault/logic/providers/password_provider.dart';
 import '../features/secure_notes/logic/providers/notes_provider.dart';
+
 class SecureVaultApp extends StatelessWidget {
   const SecureVaultApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-  providers: [
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => PasswordProvider(),
+        ),
 
-    ChangeNotifierProvider(
-      create: (_) => NotesProvider(),
-    ),
-  ],
+        ChangeNotifierProvider(
+          create: (_) => NotesProvider(),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'SecureVault AI',
