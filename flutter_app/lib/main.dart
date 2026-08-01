bc093ce6f0bf259a16a1268ae75c4319a9804e60
Kavyavineela <kavyavineelakillada@gmail.com>
2026-08-01 21:42:03 +0530

    WIP: Hive integration for Password Vault and Secure Notes

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 import 'config/app.dart';
 import 'features/password_vault/domain/models/password_model.dart';
+import 'features/secure_notes/domain/models/note_model.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,8 +11,10 @@ Future<void> main() async {
   await Hive.initFlutter();
 
   Hive.registerAdapter(PasswordModelAdapter());
+  Hive.registerAdapter(NoteModelAdapter());
 
   await Hive.openBox<PasswordModel>('passwords');
+  await Hive.openBox<NoteModel>('notes');
 
   runApp(const SecureVaultApp());
 }
